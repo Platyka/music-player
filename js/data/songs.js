550bccdc58d3c0cf4eabacd2319b536beed33446
Ambrus Attila <attilawow91@gmail.com>
2018-08-01 19:02:59 +0300

    add ./ to songs.js

--- js/data/songs.js
+++ js/data/songs.js
@@ -1,119 +1,119 @@
 export const songsList = [
   {
     "id": 1,
-    "url":"../../music/01.Just.mp3",
+    "url":"./../../music/01.Just.mp3",
     "time": "03:54",
     "song": "Just",
     "artist": "Radiohead"
   },
   {
     "id": 2,
-    "url":"../../music/02.Paranoid Android.mp3",
+    "url":"./../../music/02.Paranoid Android.mp3",
     "time": "06:27",
     "song": "Paranoid Android",
     "artist": "Radiohead"
   },
   {
     "id": 3,
-    "url":"../../music/03.Karma Police.mp3",
+    "url":"./../../music/03.Karma Police.mp3",
     "time": "04:24",
     "song": "Karma Police",
     "artist": "Radiohead"
   },
   {
     "id": 4,
-    "url":"../../music/04.Creep.mp3",
+    "url":"./../../music/04.Creep.mp3",
     "time": "03:57",
     "song": "Creep",
     "artist": "Radiohead"
   },
   {
     "id": 5,
-    "url":"../../music/05.No Surprises.mp3",
+    "url":"./../../music/05.No Surprises.mp3",
     "time": "03:49",
     "song": "No Surprises",
     "artist": "Radiohead"
   },
   {
     "id": 6,
-    "url":"../../music/06.High And Dry.mp3",
+    "url":"./../../music/06.High And Dry.mp3",
     "time": "04:17",
     "song": "High And Dry",
     "artist": "Radiohead"
   },
   {
     "id": 7,
-    "url":"../../music/07.My Iron Lung.mp3",
+    "url":"./../../music/07.My Iron Lung.mp3",
     "time": "04:36",
     "song": "My Iron Lung",
     "artist": "Radiohead"
   },
   {
     "id": 8,
-    "url":"../../music/08.There There.mp3",
+    "url":"./../../music/08.There There.mp3",
     "time": "05:22",
     "song": "There There",
     "artist": "Radiohead"
   },
   {
     "id": 9,
-    "url":"../../music/09.Lucky.mp3",
+    "url":"./../../music/09.Lucky.mp3",
     "time": "04:19",
     "song": "Lucky",
     "artist": "Radiohead"
   },
   {
     "id": 10,
-    "url":"../../music/10.Optimistic.mp3",
+    "url":"./../../music/10.Optimistic.mp3",
     "time": "04:54",
     "song": "Optimistic",
     "artist": "Radiohead"
   },
   {
     "id": 11,
-    "url":"../../music/11.Fake Plastic Trees.mp3",
+    "url":"./../../music/11.Fake Plastic Trees.mp3",
     "time": "04:51",
     "song": "Fake Plastic Trees",
     "artist": "Radiohead"
   },
   {
     "id": 12,
-    "url":"../../music/12.Idiotesque.mp3",
+    "url":"./../../music/12.Idiotesque.mp3",
     "time": "04:37",
     "song": "Idiotesque",
     "artist": "Radiohead"
   },
   {
     "id": 13,
-    "url":"../../music/13.2+2=5.mp3",
+    "url":"./../../music/13.2+2=5.mp3",
     "time": "03:19",
     "song": "2+2=5",
     "artist": "Radiohead"
   },
   {
     "id": 14,
-    "url":"../../music/14.The Bends.mp3",
+    "url":"./../../music/14.The Bends.mp3",
     "time": "04:05",
     "song": "The Bends",
     "artist": "Radiohead"
   },
   {
     "id": 15,
-    "url":"../../music/15.Pyramid Song.mp3",
+    "url":"./../../music/15.Pyramid Song.mp3",
     "time": "04:49",
     "song": "Pyramid Song",
     "artist": "Radiohead"
   },
   {
     "id": 16,
-    "url":"../../music/16.Street Spirit (Fade Out).mp3",
+    "url":"./../../music/16.Street Spirit (Fade Out).mp3",
     "time": "04:13",
     "song": "Street Spirit",
     "artist": "Radiohead"
   },
   {
     "id": 17,
-    "url":"../../music/17.Everything In Its Right Place.mp3",
+    "url":"./../../music/17.Everything In Its Right Place.mp3",
     "time": "04:11",
     "song": "Everything In Its Right Place",
     "artist": "Radiohead"
