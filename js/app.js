a524b508337e242603871a40ae5a3b09493ab1a8
Ambrus Attila <attilawow91@gmail.com>
2018-08-01 18:12:24 +0300

    something

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
 import PlayList from "./modules/playlist.js";
 import Player from "./modules/player.js";
 import Search from "./modules/search.js";
+import Trackbar from "./modules/trackbar.js";
+import Timer from "./modules/timer.js";
 
 PlayList.init();
 Player.init();
 Search.init();
+Trackbar.init();
+Timer.init();
